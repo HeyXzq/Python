8252fbcd237ae3e312924874b96a13b14b61933a
Xu Ziqi <xuziqi050501@gmail.com>
2022-05-23 23:20:57 +0800

    add grayscale

--- image_read_write.py
+++ image_read_write.py
@@ -1,7 +1,14 @@
 import cv2
 
+image = cv2.imread('./cartoon.jpg', cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('grayscale image', image)
+
+cv2.waitKey(3000)
+
+
 img = cv2.imread('./cartoon.jpg')
 
 cv2.imshow('my example image', img)
 
-cv2.waitKey(0)
+cv2.waitKey(3000)
